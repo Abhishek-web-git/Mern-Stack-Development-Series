161969ec0dea7082b2b82332cc9f6122057e92ac
Abhiraj <Abhiraj@gmail.com>
2026-06-11 02:45:07 +0530

    first commit

---
+++ JavaScript/Day-01/chai.js
@@ -0,0 +1,16 @@
+/*
+Variables in Javascript.
+*/
+const accountId = 21547
+let accountHolderName = "Abhishek Kumar"
+var accountEmail = "abhi@gmail.com"
+accountCity = "Bihar"
+
+ accountHolderName = "Vivek Kumar"
+ accountEmail = "vivek@gmail.com"
+ accountCity = "Sitamarhi"
+  let accountPassword;                // this is undefined variable.
+
+ console.table([accountId, accountHolderName, accountEmail, accountCity, accountPassword])
+
+ // prefer not to use Var becouse of issue in block scope and funcational scope.
---
+++ JavaScript/Day-01/script.js
@@ -0,0 +1,60 @@
+// 1. Variable in JavaScript...
+// variable ko Javascript me koi tarah se likh jata h 
+
+// First One.
+
+let name = "AbhisheK Kumar";
+let age = "21";
+let Address = "Bihar";
+name = "Shobha Kumari"   // yaha value ko change kar sakte h
+
+console.log(name, age, Address);
+
+// Second Type..
+
+const account = 12345;    // const me ka value fixed hoga. wo kabhi change nahi karega.
+console.log(account);  
+
+// Old Type
+var a = 20;
+var a = 40  // ye wala method humlog use nahi karte h. kyuki ek hi variable ko hum baar baar likh sakte h jo abhi galat h
+console.log(a)
+
+
+// 2. Now We Talk about Data Types.
+
+   // There are two Data Types.
+   
+   //Primitive Data Types. in this data type have 7 data types is there
+   //number, String, Boolean, bigint, null, undefined, symbol
+
+   // number
+   let b = 254;
+   let num = 25;
+   console.log(b, num);
+
+   //String
+   let title = "Abhishek Kumar"
+   let zone = "Red-Zone"
+   console.log(title, zone)
+
+   //Boolean
+   
+    let p = true;
+    let f = false;
+    console.log(p, f)
+   
+
+   //undefined
+   let user;
+   console.log(user)
+
+   //null
+   let data = null
+   console.log(data)
+   
+
+
+
+   //Non-Primitive Data Types. in this data type have 3.
+   // array, object, function.
